f380c3ac8a6a68348d4990385c73be9d2c420762
Shrey Shah <shrey.shah0610@gmail.com>
2022-06-22 12:39:01 +0530

    Added Schema

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors')
-
+const mongoose = require('mongoose')
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 app.use(require('./routers/index'))
+
+mongoose.connect('mongodb+srv://shreyshah:Googleit%40123@cluster0.fesqe.mongodb.net/?retryWrites=true&w=majority',() =>{
+    console.log('connected')
+});
 
 app.listen(3000)
---
+++ schema/schema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+
+    date : Object,
+    biryani : Number,
+    butterChicken: Number,
+    shahiPaneer: Number,
+    naan: Number,
+    roti: Number,
+    dal: Number,
+    rice: Number,
+    choleBhature: Number,
+    pizza: Number,
+    burger: Number
+});
