862a6f261708688dcd9b47f7dd9a19563008f3e9
Nachiket <nachiketdodia33@gmail.com>
2022-06-22 14:26:49 +0530

    Merged index.js with schema

--- js/index.js
+++ js/index.js
@@ -1,18 +1,6 @@
-class Order{
-    constructor(biryani, butterChicken, shahiPaneer, naan, roti, dal, rice, choleBhature, pizza, burger)
-    {
-        this.biryani = biryani;
-        this.butterChicken = butterChicken;
-        this.shahiPaneer = shahiPaneer;
-        this.naan = naan;
-        this.roti = roti;
-        this.dal = dal;
-        this.rice = rice;
-        this.choleBhature = choleBhature;
-        this.pizza = pizza;
-        this.burger = burger;
-    }
-}
+const server=require('express');
+const Order=require('../schema/schema.js');
+
 const prices={"0":200,"1":240,"2":150,"3":30,"4":350,"5":100,"6":55,"7":120,"8":150,"9":190}
 const orderTaken=()=>{
     let sum=0;
@@ -23,18 +11,6 @@ const orderTaken=()=>{
     }
     const currentOrder= new Order(f[0],f[1],f[2],f[3],f[4],f[5],f[6],f[7],f[8],f[9]);
     console.log(currentOrder);
-
-    fetch('http://localhost:3000/order',{
-        method : "POST",
-        headers: {
-            'Content-Type' : 'application/json' 
-        },
-        body: JSON.stringify(currentOrder)
-    })
-    .then(res => {
-        return res.json();
-    })
-
     console.log(JSON.stringify(currentOrder));
     console.log(sum*1.05);
 }
@@ -48,6 +24,32 @@ const orderPrice=()=>{
 }
 
 
+// class Order{
+//     constructor(biryani, butterChicken, shahiPaneer, naan, roti, dal, rice, choleBhature, pizza, burger)
+//     {
+//         this.biryani = biryani;
+//         this.butterChicken = butterChicken;
+//         this.shahiPaneer = shahiPaneer;
+//         this.naan = naan;
+//         this.roti = roti;
+//         this.dal = dal;
+//         this.rice = rice;
+//         this.choleBhature = choleBhature;
+//         this.pizza = pizza;
+//         this.burger = burger;
+//     }
+// }
+
+    // fetch('http://localhost:3000/order',{
+    //     method : "POST",
+    //     headers: {
+    //         'Content-Type' : 'application/json' 
+    //     },
+    //     body: JSON.stringify(currentOrder)
+    // })
+    // .then(res => {
+    //     return res.json();
+    // })
 
 
 
@@ -59,7 +61,7 @@ const orderPrice=()=>{
 
 
 //{
-//     "burger":320,
-//     "biryani":0,
+    //     "burger":320,
+    //     "biryani":0,
 //     "pizza":120
 //}
