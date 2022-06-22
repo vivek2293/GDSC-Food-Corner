e86125bc70bed2e4b6dbfff860d6f58a5d07217d
vivek2293 <vivek22935@gmail.com>
2022-06-22 11:07:47 +0530

    Added Fetch api

--- js/index.js
+++ js/index.js
@@ -23,6 +23,18 @@ const orderTaken=()=>{
     }
     const currentOrder= new Order(f[0],f[1],f[2],f[3],f[4],f[5],f[6],f[7],f[8],f[9]);
     console.log(currentOrder);
+
+    fetch('http://localhost:3000/order',{
+        method : "POST",
+        headers: {
+            'Content-Type' : 'application/json' 
+        },
+        body: JSON.stringify(currentOrder)
+    })
+    .then(res => {
+        return res.json();
+    })
+
     console.log(JSON.stringify(currentOrder));
     console.log(sum*1.05);
 }
