e9f09bc862ca14e1ec7d1f10dcbcaac5b95d6cfd
Shrey Shah <shrey.shah0610@gmail.com>
2022-06-24 19:38:19 +0530

    Multiple qty for single item feature added

--- js/index.js
+++ js/index.js
@@ -17,11 +17,11 @@ class Order{
 
 const prices={"0":200,"1":240,"2":180,"3":35,"4":20,"5":150,"6":120,"7":110,"8":220,"9":190}
 const orderTaken=()=>{
-    let sum=0;
     let f = [0,0,0,0,0,0,0,0,0,0];
+    let sum=0;
     for(let i=0;i<10;i++){
-        if(document.getElementById(`cbid${i}`).checked==true) f[i]=prices[`${i}`];
-        sum+=f[i];
+        f[i]=Number(document.getElementById(`i${i}`).innerText);
+        sum+=(f[i]*prices[i]);
     }
     const currentOrder= new Order(f[0],f[1],f[2],f[3],f[4],f[5],f[6],f[7],f[8],f[9],sum);
     console.log(currentOrder);
@@ -39,6 +39,12 @@ const orderTaken=()=>{
     console.log(sum*1.05);
 }
 
+function plus1(ele){
+    const parent = ele.parentNode;
+    const num = parent.childNodes[1];
+    let value = Number(num.innerText);
+    // prices[(num.id)[1]]
+}
 const orderPrice=()=>{
     let sum=0;
     for(let i=0;i<10;i++){
