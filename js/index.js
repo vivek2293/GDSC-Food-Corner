ef9df6fb2c6729f9305b14e4f53210beb91d93fa
vivek2293 <vivek22935@gmail.com>
2022-06-24 18:51:39 +0530

    Removing Checkboxes

--- js/index.js
+++ js/index.js
@@ -48,6 +48,23 @@ const orderPrice=()=>{
     console.log(sum);
 }
 
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num");
+let a = 0;
+plus.addEventListener("click", ()=>{
+    a++;
+    a = (a < 10) ? "0" + a : a;
+    num.innerText = a;
+});
+
+minus.addEventListener("click", ()=>{
+    if(a > 0){
+    a--;
+    a = (a < 10) ? "0" + a : a;
+    num.innerText = a;
+    }
+});
 
 
     
