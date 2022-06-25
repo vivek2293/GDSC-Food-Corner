b1b278bdec4e3132d060eaf2405ea0c213bd9f64
Shrey Shah <shrey.shah0610@gmail.com>
2022-06-25 16:12:46 +0530

    Formatted Codebase

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+// inporting modules
 const express = require('express');
 const cors = require('cors')
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//  Routing 
+
 app.use(require('./routers/server.js'));
 app.use(require('./admin/admin.js'));
 
+//  Admin side port
 app.listen(3000);
--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,11 @@
+// Importing modules
 const mongoose = require('mongoose');
+
+// Creating schema of the qty of each item and cost having total price of order
 
 const PostSchema = mongoose.Schema({
     date: Object,
-    biryani : Number,
+    biryani: Number,
     butterChicken: Number,
     shahiPaneer: Number,
     naan: Number,
@@ -12,7 +15,7 @@ const PostSchema = mongoose.Schema({
     choleBhature: Number,
     pizza: Number,
     burger: Number,
-    cost:Number
+    cost: Number
 });
 
-module.exports = mongoose.model('Order',PostSchema);
+module.exports = mongoose.model('Order', PostSchema);
