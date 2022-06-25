09119951c8083a52d050995e9f484cb9c131c155
vivek2293 <vivek22935@gmail.com>
2022-06-25 09:11:47 +0530

    Formatted CodeBase

--- admin/admin.js
+++ admin/admin.js
@@ -1,19 +1,19 @@
-const express=require('express');
-const admin=express();
+const express = require('express');
+const admin = express();
 const mongoose = require('mongoose')
-const Order=require('../schema/schema.js')
+const Order = require('../schema/schema.js')
 require('dotenv').config();
-mongoose.connect(process.env.DB_CONNECTION,() =>{
+mongoose.connect(process.env.DB_CONNECTION, () => {
     console.log('connected')
 });
 
-admin.set('view engine','ejs');
-admin.set('views','admin/views');
+admin.set('view engine', 'ejs');
+admin.set('views', 'admin/views');
 
-admin.get('/admin',async (req,res)=>{
-    const orders= await Order.find();
-    res.render('index',{orders});
+admin.get('/admin', async (req, res) => {
+    const orders = await Order.find();
+    res.render('index', { orders });
     // console.log(orders);
 });
 
-module.exports=admin;
+module.exports = admin;
--- js/custom_slick.js
+++ js/custom_slick.js
@@ -1,31 +1,31 @@
 $('.multiple-items').slick({
-    infinite: true,
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: false,
-    autoplay: true,
-    autoplayspeed: 1000,
-    dots: false,
-    centerPadding: '0px',
-    responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-            dots: false
-          }
-        },
-        {
-            breakpoint: 800,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true
-            }
-        },
-      ]
+  infinite: true,
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplayspeed: 1000,
+  dots: false,
+  centerPadding: '0px',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+  ]
 });
